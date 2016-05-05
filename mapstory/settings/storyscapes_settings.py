5f2bce49f7330471260c089f25403765d9be4245
Jorge Martinez <jorgemg0327@gmail.com>
2016-05-05 16:33:13 -0500

    Using storyscapes configuration file

--- mapstory/settings/storyscapes_settings.py
+++ mapstory/settings/storyscapes_settings.py
@@ -3,5 +3,5 @@ from mapstory.settings.base import *
 SITENAME = 'Storyscapes'
 
 TEMPLATE_DIRS = (
-    os.path.join(LOCAL_ROOT, "templates_old"),
+    os.path.join(LOCAL_ROOT, "templates_sc"),
 ) + TEMPLATE_DIRS
